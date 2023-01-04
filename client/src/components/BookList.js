945d8584c683be3020cbddf977d77cdab14c8d6c
hareesh.rm <hareesh.rm@zohocorp.com>
2023-01-04 16:28:07 +0530

    feat : Added css

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -2,20 +2,29 @@ import "../App.css";
 import { graphql } from "react-apollo";
 import { getBooksQuery } from "../queries/queries";
 
+//components
+
+import BookDetails from "./BookDetails";
+import { useState } from "react";
+
 function BookList(props) {
+  const [selected, setSelected] = useState(null)
   const displayBooks = () => {
     var data = props.data;
     if (data.loading) {
       return <div> Loading books...</div>;
     } else {
       return data.books.map((book) => {
-        return <li key={book.id}> {book.name}</li>;
+        return <li key={book.id} onClick={(e)=>{
+          setSelected(book.id)
+        }}> {book.name}</li>;
       });
     }
   };
   return (
     <div>
       <ul id="book-list">{displayBooks()}</ul>
+      <BookDetails bookId={selected}/>
     </div>
   );
 }
