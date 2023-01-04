5e8dece7c258396c262a5a1a60fe10d6cdad24a3
hareesh.rm <hareesh.rm@zohocorp.com>
2023-01-04 14:43:59 +0530

    feat : Rendering data from db

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,23 +1,30 @@
 import "../App.css";
-import {gql} from 'apollo-boost'
-import {graphql} from 'react-apollo'
+import { gql } from "apollo-boost";
+import { graphql } from "react-apollo";
 
 const getBooksQuery = gql`
   {
-    books{
+    books {
       name
       id
     }
   }
-`
+`;
 
 function BookList(props) {
-  console.log(props)
+  const displayBooks = () => {
+    var data = props.data;
+    if (data.loading) {
+      return <div> Loading books...</div>;
+    } else {
+      return data.books.map((book) => {
+        return <li key={book.id}> {book.name}</li>;
+      });
+    }
+  };
   return (
     <div>
-      <ul id="book-list">
-        <li>Book Name</li>
-      </ul>
+      <ul id="book-list">{displayBooks()}</ul>
     </div>
   );
 }
