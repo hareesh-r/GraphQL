0665832a808a6fb652bf33b899d8549ad6d7cf4b
hareesh.rm <hareesh.rm@zohocorp.com>
2023-01-04 15:13:10 +0530

    feat : Extracted queries to seperate file

---
+++ client/src/components/AddBook.js
@@ -0,0 +1,37 @@
+import "../App.css";
+import { graphql } from "react-apollo";
+import { getAuthorsQuery } from "../queries/queries";
+
+function AddBook(props) {
+    const displayAuthors = () => {
+        var data = props.data;
+        if(data.loading) 
+            return (<option disabled>Loading Authors...</option>);
+        else 
+            return (data.authors.map(author=>{ 
+                return (<option key={author.id} value={author.id}>{author.name}</option>)
+            }))
+    } 
+  return (
+    <form id="add-book">
+      <div className="field">
+        <label>Book name:</label>
+        <input type="text" />
+      </div>
+      <div className="field">
+        <label>Genre:</label>
+        <input type="text" />
+      </div>
+      <div className="field">
+        <label>Author:</label>
+        <select>
+          <option>Select author</option>
+          {displayAuthors()}
+        </select>
+      </div>
+      <button>+</button>
+    </form>
+  );
+}
+
+export default graphql(getAuthorsQuery)(AddBook);
